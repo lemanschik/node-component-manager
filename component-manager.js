c59942e2414fea39652dc3356742411deec2a745
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2023-03-23 18:42:01 +0100

    Create component-manager.js

---
+++ component-manager.js
@@ -0,0 +1,15 @@
+/**
+ * Main Entrypoint
+ */ 
+
+class ComponenetManager {
+  // maybe contains= strip that equal sign use shellparsing. JS/ZSH/BASH not needed with NodeJS process.argv handels that
+  
+  '--start': ()=>{}
+}
+
+for (let key of Object.keys(ComponentManager.prototype)) {
+  process.argv.contains(key) && 
+}
+
+export { }
